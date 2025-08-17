fa20dbd1bc8827c2d54cc8b2e35b3c6479851b96
ASkovorodker <108782450+ASkovorodker@users.noreply.github.com>
2025-08-17 17:47:35 +0500

    Delete src directory

--- src/create_json_file.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def create_json_file(found_substring):
-        with open("output.json", "w", encoding="utf-8") as f:
-            json.dump(found_substring, f, ensure_ascii=False, indent=2)
-
-        return "Подстрока найдена.\nJSON файл создан."
--- src/validation.py
+++
@@ -1,6 +0,0 @@
-def validation(strings_array):
-    if len(strings_array) <= 2:
-        code = 1
-    else:
-        code = 0
-    return code
