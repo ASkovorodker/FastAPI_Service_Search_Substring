986002e2e1f94e9fdc73b905f30ce7872a40cb74
ASkovorodker <108782450+ASkovorodker@users.noreply.github.com>
2025-08-17 16:39:09 +0500

    Creating JSON file with found substring

--- src/validation.py
+++ src/validation.py
@@ -1,12 +1,11 @@
 import find_longest_common_substring
+import create_json_file
 
 def validation(strings_array):
     if len(strings_array) <= 2:
         mssg = "Длина массива 2 или меньше: поиск подстрок не выполняется"
     else:
-        mssg = "Найдена общая подстрока: "
-        # Находим самую длинную подстроку в массиве и возвращаем ее
+        # Находим самую длинную подстроку в массиве и записываем ее в файл
         found_substring = find_longest_common_substring.find_longest_common_substring(strings_array)
-        # Выводим найденную подстроку в консоль
-        mssg += found_substring
+        mssg = create_json_file.create_json_file(found_substring)
     return mssg
